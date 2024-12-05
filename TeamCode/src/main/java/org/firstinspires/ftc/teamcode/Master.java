fb1ba98aa21b767c98857acc98498a932db4bea3
25Kero <25kero@decorah.school>
2024-12-05 10:47:16 -0600

    added telemetry data for debugging and added dpad Support

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Master.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Master.java
@@ -5,8 +5,13 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
-@TeleOp(name = "Driver Teleop")
+@TeleOp(name = "Driver Teleop 24-25")
 public class Master extends LinearOpMode {
+    double rightX;
+    double leftX;
+    double rightY;
+    double leftY;
+
     private teamRobot Robot = new teamRobot();
     private boolean precisionDrive = false;
 
@@ -31,14 +36,19 @@ public class Master extends LinearOpMode {
             driveControl();
             liftControl();
             clawControl();
+
+            telemetry.addData("rightX", rightX);
+            telemetry.addData("leftX", leftX);
+            telemetry.addData("rightY", rightY);
+            telemetry.addData("lightY", leftY);
+            telemetry.update();
         }
     }
-    // controls the wheels for driving
+
+    /**
+     * controls wheels for driving
+     */
     private void driveControl() {
-         double rightX;
-         double leftX;
-         double rightY;
-         double leftY;
 
         if (gamepad1.left_bumper) {
             precisionDrive = false;
@@ -46,20 +56,17 @@ public class Master extends LinearOpMode {
         if (gamepad1.right_bumper) {
             precisionDrive = true;
         }
-
+        rightX = gamepad1.right_stick_x;
+        rightY = gamepad1.right_stick_y;
+        leftX = gamepad1.left_stick_x;
+        leftY = gamepad1.left_stick_y;
         // if precsisonDrive, then set values to 1/4 their value for the robot to move 1/4 the speed
         if (precisionDrive) {
-            rightX = gamepad1.right_stick_x / 3;
-            rightY = gamepad1.right_stick_y / 3;
-            leftX = gamepad1.left_stick_x / 3;
-            leftY = gamepad1.left_stick_y / 3;
-        } else {
-            rightX = gamepad1.right_stick_x;
-            rightY = gamepad1.right_stick_y;
-            leftX = gamepad1.left_stick_x;
-            leftY = gamepad1.left_stick_y;
+            rightX = rightX / 3;
+            rightY = rightY / 3;
+            leftX = leftX / 3;
+            leftY = leftY / 3;
         }
-
         // set wheel power to designated values
         Robot.frontRightMotor.setPower(-(rightY + rightX));
         Robot.frontLeftMotor.setPower(leftY - leftX);
@@ -67,8 +74,10 @@ public class Master extends LinearOpMode {
         Robot.backLeftMotor.setPower(leftY + leftX);
     }
 
-    // controls the viper slide lifting system
-    // dpad up raises, dpad down lowers
+    /**
+     * controls the viper slide lifting system
+     * dpad up raises, dpad down lowers
+     */
     private void liftControl() {
         if (gamepad2.dpad_up) {
             Robot.liftMotor.setPower(-0.9);
@@ -78,29 +87,27 @@ public class Master extends LinearOpMode {
             Robot.liftMotor.setPower(-0.05);
         }
     }
-    // controls the claw servos and the motor that raises them
+
+    /**
+     * controls the claw servos and the motor that raises them
+     * right bumper opens, left bumper closes
+     * right > 0, lower claw. right < 0, raise claw
+     * x opens clipping servo, b closes clipping servo
+     */
     private void clawControl() {
-        //open and close the claw
-        // right bumper opens, left bumper closes
         if (gamepad2.right_bumper) {
             Robot.setClawState(teamRobot.ClawState.OPEN_CLAW);
         }
         if (gamepad2.left_bumper) {
             Robot.setClawState(teamRobot.ClawState.CLOSE_CLAW);
         }
-
         if (gamepad2.right_stick_y > 0) {
-            // lower claw
             Robot.clawMotor.setPower(-0.5);
         } else if (gamepad2.right_stick_y < 0) {
-            //raise claw
             Robot.clawMotor.setPower(0.6);
         } else {
             Robot.clawMotor.setPower(0.1);
         }
-
-        //open and close clipping servo
-        // x opens. b closes
         if (gamepad2.x) {
             Robot.setClipServoState(teamRobot.ClawState.OPEN_CLAW);
         }
