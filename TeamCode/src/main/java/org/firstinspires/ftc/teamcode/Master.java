3385bb8f95b5d3327f7998b801f0e404b92028b9
25Kero <153110907+25Kero@users.noreply.github.com>
2024-12-06 14:25:47 -0600

    pre-odomotry stable (#3)
    * fixed clip servo buttons. changed values for claw lift motor so it properly moves.
    
    * fixed clip servo buttons. changed values for claw lift motor so it properly moves.
    
    * added telemetry data for debugging and added dpad Support
    
    * added telemetry data for debugging and added dpad Support
    
    * added telemetry data for debugging
    
    * removed acme robotics roadrunner as it is not needed when using goBilda Odometry.
     removed dpad support temporarily.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Master.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Master.java
@@ -3,19 +3,25 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
-@TeleOp(name = "Driver Teleop")
+@TeleOp(name = "Driver Teleop 24-25")
 public class Master extends LinearOpMode {
+    double rightX;
+    double leftX;
+    double rightY;
+    double leftY;
+
     private teamRobot Robot = new teamRobot();
     private boolean precisionDrive = false;
 
     @Override
     public void runOpMode() {
         // initialize motors with configuration
-        Robot.frontRightMotor = hardwareMap.get(DcMotor.class, Robot.FRONT_RIGHT);
-        Robot.frontLeftMotor = hardwareMap.get(DcMotor.class, Robot.FRONT_LEFT);
-        Robot.backRightMotor = hardwareMap.get(DcMotor.class, Robot.BACK_RIGHT);
-        Robot.backLeftMotor = hardwareMap.get(DcMotor.class, Robot.BACK_LEFT);
+        Robot.frontRightMotor = hardwareMap.get(DcMotorEx.class, Robot.FRONT_RIGHT);
+        Robot.frontLeftMotor = hardwareMap.get(DcMotorEx.class, Robot.FRONT_LEFT);
+        Robot.backRightMotor = hardwareMap.get(DcMotorEx.class, Robot.BACK_RIGHT);
+        Robot.backLeftMotor = hardwareMap.get(DcMotorEx.class, Robot.BACK_LEFT);
         Robot.clawMotor = hardwareMap.get(DcMotor.class, Robot.CLAW_MOTOR);
         Robot.liftMotor = hardwareMap.get(DcMotor.class, Robot.LIFT);
 
@@ -30,14 +36,19 @@ public class Master extends LinearOpMode {
             driveControl();
             liftControl();
             clawControl();
+
+            telemetry.addData("rightX", rightX);
+            telemetry.addData("leftX", leftX);
+            telemetry.addData("rightY", rightY);
+            telemetry.addData("lightY", leftY);
+            telemetry.update();
         }
     }
-    // controls the wheels for driving
+
+    /**
+     * controls wheels for driving
+     */
     private void driveControl() {
-         double rightX;
-         double leftX;
-         double rightY;
-         double leftY;
 
         if (gamepad1.left_bumper) {
             precisionDrive = false;
@@ -45,20 +56,17 @@ public class Master extends LinearOpMode {
         if (gamepad1.right_bumper) {
             precisionDrive = true;
         }
-
+        rightX = gamepad1.right_stick_x;
+        rightY = gamepad1.right_stick_y;
+        leftX = gamepad1.left_stick_x;
+        leftY = gamepad1.left_stick_y;
         // if precsisonDrive, then set values to 1/4 their value for the robot to move 1/4 the speed
         if (precisionDrive) {
-            rightX = 0.25 * gamepad1.right_stick_x;
-            rightY = 0.25 * gamepad1.right_stick_y;
-            leftX = gamepad1.left_stick_x / 4;
-            leftY = gamepad1.left_stick_y / 4;
-        } else {
-            rightX = gamepad1.right_stick_x;
-            rightY = gamepad1.right_stick_y;
-            leftX = gamepad1.left_stick_x;
-            leftY = gamepad1.left_stick_y;
+            rightX = rightX / 3;
+            rightY = rightY / 3;
+            leftX = leftX / 3;
+            leftY = leftY / 3;
         }
-
         // set wheel power to designated values
         Robot.frontRightMotor.setPower(-(rightY + rightX));
         Robot.frontLeftMotor.setPower(leftY - leftX);
@@ -66,8 +74,10 @@ public class Master extends LinearOpMode {
         Robot.backLeftMotor.setPower(leftY + leftX);
     }
 
-    // controls the viper slide lifting system
-    // dpad up raises, dpad down lowers
+    /**
+     * controls the viper slide lifting system
+     * dpad up raises, dpad down lowers
+     */
     private void liftControl() {
         if (gamepad2.dpad_up) {
             Robot.liftMotor.setPower(-0.9);
@@ -77,29 +87,27 @@ public class Master extends LinearOpMode {
             Robot.liftMotor.setPower(-0.05);
         }
     }
-    // controls the claw servos and the motor that raises them
+
+    /**
+     * controls the claw servos and the motor that raises them
+     * right bumper opens, left bumper closes
+     * right > 0, lower claw. right < 0, raise claw
+     * x opens clipping servo, b closes clipping servo
+     */
     private void clawControl() {
-        //open and close the claw
-        // right bumper opens, left bumper closes
         if (gamepad2.right_bumper) {
             Robot.setClawState(teamRobot.ClawState.OPEN_CLAW);
         }
         if (gamepad2.left_bumper) {
             Robot.setClawState(teamRobot.ClawState.CLOSE_CLAW);
         }
-
-        // lower and raise claw
-        // values for sticks greater than zero mean user is pulling the stick down, vice versa
         if (gamepad2.right_stick_y > 0) {
-            Robot.clawMotor.setPower(0);
+            Robot.clawMotor.setPower(-0.5);
         } else if (gamepad2.right_stick_y < 0) {
             Robot.clawMotor.setPower(0.6);
         } else {
-            Robot.clawMotor.setPower(0.2);
+            Robot.clawMotor.setPower(0.1);
         }
-
-        //open and close clipping servo
-        // x opens. b closes
         if (gamepad2.x) {
             Robot.setClipServoState(teamRobot.ClawState.OPEN_CLAW);
         }
